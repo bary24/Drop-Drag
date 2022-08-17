264682fb8c2a65932c73790e31486f4dbc585e4b
bary24 <ahmed.bary@icloud.com>
2022-08-17 19:18:38 +0200

    editing gitignore

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-import { ProjectInput } from "./components/project-input.js";
-import { ProjectList } from "./components/project-list.js";
-////////////////////////////////    Classes Section ////////////////////////////////
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
